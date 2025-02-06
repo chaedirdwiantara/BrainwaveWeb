9cc7efc50ad1478f26b8ad8e358345d34903d3c1
chaedirdwiantara <chaedirdwiantara@gmail.com>
2025-02-06 17:15:19 +0700

    add company logos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ import { heroIcons } from "../contants"
 import { useRef } from "react"
 import Generating from "./Generating"
 import Notification from "./Notification"
+import CompanyLogos from "./CompanyLogos"
 
 const Hero = () => {
     const parallaxRef = useRef(null)
@@ -89,6 +90,8 @@ const Hero = () => {
 
                     <BackgroundCircles />
                 </div>
+
+                <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
             </div>
 
             <BottomLine />
